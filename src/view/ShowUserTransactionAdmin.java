c95b167f352ccffbe8c1833ad933236e27b439af
Jonathan Sena <jonathansena798@gmail.com>
2023-11-23 22:53:31 +0700

    pain

--- src/view/ShowUserTransactionAdmin.java
+++ src/view/ShowUserTransactionAdmin.java
@@ -3,12 +3,16 @@ package view;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JSeparator;
+import javax.swing.JTextField;
 
 import model.Admin;
+import model.User;
 
 /**
  *
@@ -16,9 +20,9 @@ import model.Admin;
  */
 public class ShowUserTransactionAdmin {
     JFrame container;
-    JButton btnBack;
+    JButton btnBack, btnSelect;
 
-    public ShowUserTransactionAdmin(Admin admin) {
+    public ShowUserTransactionAdmin(Admin admin, ArrayList<User> users) {
         container = new JFrame("Show User Transaction");
         container.setSize(480, 300);
         container.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,14 +35,40 @@ public class ShowUserTransactionAdmin {
         title.setForeground(Color.WHITE);
         container.add(title);
 
+        String[] columnNames = {"ID", "Name", "Status", "Wallet"};
+
+        Object[][] data = new Object[users.size()][4]; 
+
+        for (int i = 0; i < users.size(); i++) {
+            User user = users.get(i);
+            data[i][0] = user.getId(); 
+            data[i][1] = user.getName(); 
+            data[i][2] = user.getStatus().toString();
+            data[i][3] = user.getWallet(); 
+        }
         JSeparator garisPemisah = new JSeparator();
         garisPemisah.setBounds(28, 150, 410, 5);
         garisPemisah.setForeground(Color.LIGHT_GRAY);
         container.add(garisPemisah);
 
+        JLabel inputid = new JLabel("User ID");
+        inputid.setBounds(28, 170, 100, 5);
+        inputid.setForeground(Color.WHITE);
+        inputid.setBackground(Color.decode("#717D7E"));
+        JTextField input = new JTextField();
+        input.setBounds(150, 170, 200, 5);
+        input.setForeground(Color.WHITE);
+        input.setBackground(Color.DARK_GRAY);
+
+        btnSelect = new JButton("Select");
+        btnSelect.setBounds(370, 170, 150, 23);
+        btnSelect.setForeground(Color.WHITE);
+        btnSelect.setBackground(Color.decode("#717D7E"));
+        container.add(btnSelect);
+
         //Bagian Button Back
         btnBack = new JButton("Back");
-        btnBack.setBounds(270, 182, 150, 23);
+        btnBack.setBounds(270, 200, 150, 23);
         btnBack.setForeground(Color.WHITE);
         btnBack.setBackground(Color.decode("#717D7E"));
         container.add(btnBack);
